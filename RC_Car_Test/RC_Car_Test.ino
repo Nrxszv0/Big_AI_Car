b1a7d7d434d64765c9950e77b5b6d5b5051e9326
Michael <michael.crothersmc@icloud.com>
2020-07-04 00:53:07 -0500

    It doesn't work for some reason

--- RC_Car_Test/RC_Car_Test.ino
+++ RC_Car_Test/RC_Car_Test.ino
@@ -1,25 +1,28 @@
 #include <Servo.h>
-int serPin = 7, serVal=115; //30, 115
-int escPin = 6, escVal=10; //95 150?
+int serPin = 7, serVal = 115; //30, 115
+int escPin = 6, escVal = 10; //95 150?
 int startEscSpeed = 110;
 Servo ser;
 Servo esc;
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-ser.attach(serPin);
-esc.attach(escPin);
+  Serial.begin(9600);
+  ser.attach(serPin);
+  esc.attach(escPin);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  ser.write(115);
-delay(500);
-ser.write(50);
-delay(500);
-//esc.write(0);
-//esc.write(0);
-//delay(10000);
-//delay(10000000);
+//  ser.write(115);
+//  delay(500);
+//  ser.write(50);
+//  delay(500);
+  esc.write(100);
+  delay(500);
+  esc.write(0);
+  ser.write(70);
+  delay(500);
+  esc.write(100);
 
 }
