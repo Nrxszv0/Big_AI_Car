71c67ff50cfb7355ad715b4c8e54168a0f455c7e
Michael <michael.crothersmc@icloud.com>
2020-07-04 00:46:09 -0500

    The servo doesn't turn

--- RC_Car_Test/RC_Car_Test.ino
+++ RC_Car_Test/RC_Car_Test.ino
@@ -1,14 +1,29 @@
 #include <Servo.h>
 int serPin = 7, serVal=115; //30, 115
+int escPin = 6, escVal=10; //95 150?
+int startEscSpeed = 110;
 Servo ser;
+Servo esc;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 ser.attach(serPin);
+esc.attach(escPin);
+//esc.write(startEscSpeed);
+//delay(30);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-ser.write(serVal);
+//ser.write(serVal);
+//esc.write(90);
+ser.write(115);
+//delay(500);
+//ser.write(100);
+//delay(500);
+//esc.write(0);
+//esc.write(0);
+//delay(10000);
+//delay(10000000);
 
 }
