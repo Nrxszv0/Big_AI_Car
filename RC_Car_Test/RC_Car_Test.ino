cb462a8339fdee31fb2aef6c8981820cf4b70317
Michael <michael.crothersmc@icloud.com>
2020-07-14 18:59:11 -0500

    Added both servos and ultras

--- RC_Car_Test/RC_Car_Test.ino
+++ RC_Car_Test/RC_Car_Test.ino
@@ -2,14 +2,18 @@
 int serPin = 7, serVal = 115; //30, 115
 int escPin = 6, escVal = 10; //95 150?.  //Around 70 = Stop. 80+ = Forwards(81 = slowest moving speed. 110+ = fastest speed). 65- = Backwards (anything below 55 is top reverse speed)
 int startEscSpeed = 110;
-int serPin1 = 5, serVal1;
-int dly = 1000;
+int serPin1 = 4, serVal1;
+int serPin2 = 5, serVal2;
+int minAng = 0, maxAng = 140, minAng2 = 180, maxAng2 = 40;
+int dly = 15;
+String angMsg = "Angle ", angMsg2 = ": ";
 Servo ser;
 Servo esc;
 Servo ser1;
+Servo ser2;
 
 int echoPin = 9, trigPin = 8;
-int ssDly = 2, sDly = 20;
+int ssDly = 2, sDly = 100;
 float distanceIn;
 void setup() {
   // put your setup code here, to run once:
@@ -17,14 +21,35 @@ void setup() {
   ser.attach(serPin);
   esc.attach(escPin);
   ser1.attach(serPin1);
-  ser1.write(0);
+  ser2.attach(serPin2);
+  ser1.write(140); // 0 = Forward 90 = Left 180 =Back
+  ser2.write(40); // 180 = Forward 90 = Right 0 = Back
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(distanceTest());
+  for ( int i = minAng; i <= maxAng; i++) {
+    ser1.write(i);
+    ser2.write(minAng2 - i);
+    Serial.print(i);
+    Serial.print("    ");
+    Serial.println(minAng2 - i);
+    delay(dly);
+  }
+  delay(500);
+  for ( int i = maxAng; i >= 0; i--) {
+    ser1.write(i);
+    ser2.write(minAng2 - i);
+    Serial.print(i);
+    Serial.print("    ");
+    Serial.println(minAng2 - i);
+    delay(dly);
+  }
+  delay(500);
+
+
+
   //   esc.write(110);
   //   delay(3500);
   //   esc.write(70);
--- somebodyscarcode/somebodyscarcode.ino
+++ somebodyscarcode/somebodyscarcode.ino
@@ -5,8 +5,8 @@ const int esc = 12;       // first servo
 Servo myesc;  // create servo object to control a servo
 const int buttonPin = 13;     // the number of the pushbutton pin
 int buttonState = 0;
-const int ledpin = 2; 
-char data = 0; 
+const int ledpin = 2;
+char data = 0;
 int driverState = 0;
 int servoval = 100;
 
@@ -24,22 +24,22 @@ long duration, distance, distance1, distance2, distance3, distance4, difference,
 void setup() {
   // put your setup code here, to run once:
 
-myservo1.attach(servo1);  // attaches the servo
-myesc.attach(esc);  // attaches the servo
-pinMode(buttonPin, INPUT);
-pinMode(ledpin, OUTPUT);
- pinMode(trigD1, OUTPUT);
+  myservo1.attach(servo1);  // attaches the servo
+  myesc.attach(esc);  // attaches the servo
+  pinMode(buttonPin, INPUT);
+  pinMode(ledpin, OUTPUT);
+  pinMode(trigD1, OUTPUT);
   pinMode(echoD1, INPUT);
-   pinMode(trigD2, OUTPUT);
+  pinMode(trigD2, OUTPUT);
   pinMode(echoD2, INPUT);
-   pinMode(trigD3, OUTPUT);
+  pinMode(trigD3, OUTPUT);
   pinMode(echoD3, INPUT);
   pinMode(trigD4, OUTPUT);
   pinMode(echoD4, INPUT);
 
-myesc.write(80);
-myservo1.write(100);
-Serial.begin(9600);
+  myesc.write(80);
+  myservo1.write(100);
+  Serial.begin(9600);
 }
 
 
@@ -47,149 +47,149 @@ void loop() {
 
 
 
-SonarSensor(trigD1, echoD1);
-distance1 = distance;
-SonarSensor(trigD2, echoD2);
-distance2 = distance;
-SonarSensor(trigD3, echoD3);
-distance3 = distance;
-SonarSensor(trigD4, echoD4);
-distance4 = distance;
+  SonarSensor(trigD1, echoD1);
+  distance1 = distance;
+  SonarSensor(trigD2, echoD2);
+  distance2 = distance;
+  SonarSensor(trigD3, echoD3);
+  distance3 = distance;
+  SonarSensor(trigD4, echoD4);
+  distance4 = distance;
 
 
-  if (distance1 >= 300 || distance1 < 0){
+  if (distance1 >= 300 || distance1 < 0) {
     distance1 = 0;
   }
 
-  if (distance2 >= 300 || distance2 < 0){
+  if (distance2 >= 300 || distance2 < 0) {
     distance2 = 0;
   }
 
-  if (distance3 >= 300 || distance3 < 0){
+  if (distance3 >= 300 || distance3 < 0) {
     distance3 = 0;
   }
-  if (distance4 >= 300 || distance4 < 0){
+  if (distance4 >= 300 || distance4 < 0) {
     distance4 = 0;
   }
-  
-difference = (distance1-distance2);
-absDiff = abs(difference);
-  
-buttonState = digitalRead(buttonPin);
 
-if ((buttonState == HIGH)&&(driverState == 0))
- {
-  delay(1500);
-  driverState = 1;
- }
+  difference = (distance1 - distance2);
+  absDiff = abs(difference);
 
-if(Serial.available() > 0)      // Send data only when you receive data:
-   {
-      data = Serial.read();        //Read the incoming data & store into data
-      Serial.print(data);          //Print Value inside data in Serial monitor
-      Serial.print("\n");     
-       
-      if(data == '1') 
-      {  
-       driverState = 1;
-      }
-      if(data == '0')
+  buttonState = digitalRead(buttonPin);
+
+  if ((buttonState == HIGH) && (driverState == 0))
+  {
+    delay(1500);
+    driverState = 1;
+  }
+
+  if (Serial.available() > 0)     // Send data only when you receive data:
+  {
+    data = Serial.read();        //Read the incoming data & store into data
+    Serial.print(data);          //Print Value inside data in Serial monitor
+    Serial.print("\n");
+
+    if (data == '1')
+    {
+      driverState = 1;
+    }
+    if (data == '0')
+    {
+      driverState = 0;
+    }
+  }
+
+
+  if (driverState == 0)
+  {
+    myesc.write(80);
+    myservo1.write(100);
+    digitalWrite(ledpin, LOW);
+  }
+
+
+  if (driverState == 1)
+  {
+    //Drive code here
+    //variables: distance1, distance2, distance3 in cm
+    digitalWrite(ledpin, HIGH);
+
+    if (distance3 < 5 && distance3 != 0)
+    {
+      myesc.write(94);
+      myservo1.write(65);
+    }
+    else
+    {
+      if ((distance4 < 10 && distance4 != 0) && (distance3 >= 20 || distance3 == 0))
       {
-        driverState = 0;
+        myesc.write(92);
+        myservo1.write(130);
       }
-}
-  
+      else if ((distance4 < 10 && distance4 != 0) && (distance3 < 20 && distance3 != 0))
+      {
+        myesc.write(92);
+        myservo1.write(65);
+      }
 
-if(driverState == 0)
+      else
+      {
+        if ((distance1 == 0) || (distance1 > 45))
+        {
+
+          if ((distance3 <= 45) && (distance3 != 0))
+          {
+            myesc.write(91);
+            myservo1.write(95);
+
+            if ((distance2 <= 35) && (distance2 != 0))
+            {
+              myesc.write(92);
+              myservo1.write(70);
+            }
+          }
+          else
+          {
+            myesc.write(93);
+            myservo1.write(130);
+          }
+
+        }
+        else
+        {
+          myesc.write(91);
+          myservo1.write(130);
+        }
+
+
+      }
+    }
+
+
+  }
+  Serial.print(distance1);
+  Serial.print("\n \n distance2 \n");
+  Serial.print(distance2);
+  Serial.print("\n \n distance3 \n");
+  Serial.print(distance3);
+  Serial.print("\n \n distance1 \n");
+  //Serial.print(difference);
+  //Serial.print("\n \n \n");
+
+}
+
+
+
+
+
+void SonarSensor(int trigPin, int echoPin)
 {
-  myesc.write(80);
-  myservo1.write(100);
-  digitalWrite(ledpin, LOW);
-}
-
-
-if(driverState == 1)
-{
-//Drive code here
-//variables: distance1, distance2, distance3 in cm
-digitalWrite(ledpin, HIGH);
-
-if (distance3 < 5 && distance3 != 0)
-{
-     myesc.write(94);
-   myservo1.write(65); 
-}
-else
-{
-if((distance4 < 10 && distance4 != 0)&&(distance3 >= 20 || distance3 == 0))
-{
-    myesc.write(92);
-   myservo1.write(130); 
-}
-else if((distance4 < 10 && distance4 != 0)&&(distance3 < 20 && distance3 != 0))
-{
-    myesc.write(92);
-   myservo1.write(65); 
-}
-
-else
-{
-if((distance1 == 0) || (distance1 > 45))
-{
-
-if((distance3 <= 45)&&(distance3 != 0))
-{
-  myesc.write(91);
-   myservo1.write(95); 
-   
-   if((distance2 <= 35)&&(distance2 != 0))
-{
-    myesc.write(92);
-   myservo1.write(70); 
-}
-}
-else
-{
-  myesc.write(93);
-   myservo1.write(130); 
-}
-
-}
-else
-{
-     myesc.write(91);
-   myservo1.write(130); 
-}
-
-
-}
-}
-
-
-}
-Serial.print(distance1);
-Serial.print("\n \n distance2 \n");
-Serial.print(distance2);
-Serial.print("\n \n distance3 \n");
-Serial.print(distance3);
-Serial.print("\n \n distance1 \n");
-//Serial.print(difference);
-//Serial.print("\n \n \n");
-
-}
-
-
-
-
-
-void SonarSensor(int trigPin,int echoPin)
-{
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-duration = pulseIn(echoPin, HIGH);
-distance = (duration/2) / 29.1;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
 
 }
